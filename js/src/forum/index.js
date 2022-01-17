ac5552784579f5b04fe0c80f5d254b6e5c613519
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-01-17 17:56:02 -0500

    feat: add next/prev buttons to articles.

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,9 +1,11 @@
 import registerModels from '../common/registerModels';
 import addArticleList from './addArticleList';
+import addArticlePrevNextButtons from './addArticlePrevNextButtons';
 import addArticleSeriesModal from './addArticleSeriesModal';
 
 app.initializers.add('askvortsov/flarum-article-series', () => {
   registerModels();
   addArticleList();
   addArticleSeriesModal();
+  addArticlePrevNextButtons();
 });
